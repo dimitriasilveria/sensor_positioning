b422977288e1d060cd502ffc8e21eca5f4b488f0
Dimitria P. Silveria <63125739+dimitriasilveria@users.noreply.github.com>
2024-04-10 16:01:04 -0400

    Update train.py

--- train.py
+++ train.py
@@ -43,7 +43,7 @@ def parse_args():
     # Core training parameters
     parser.add_argument("--lr", type=float, default=0.012, help="learning rate for Adam optimizer")
     parser.add_argument("--gamma", type=float, default=0.95, help="discount factor")
-    parser.add_argument("--batch-size", type=int, default=1024, help="number of episodes to optimize at the same time")
+    parser.add_argument("--batch-size", type=int, default=512, help="number of episodes to optimize at the same time")
     parser.add_argument("--num-units", type=int, default=64, help="number of units in the mlp")
     # Checkpointing
     parser.add_argument("--exp-name", type=str, default="test", help="name of the experiment")
